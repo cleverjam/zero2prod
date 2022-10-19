3d02437db41e5e92903e8457a718c09fc511b640
Jaime Liz <lizjaime@gmail.com>
2022-10-18 20:55:15 -0500

    pausing now because actix extractors using serde blew my mind - and I only get ~10% of how it works! 🤯🤯

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,12 +2,12 @@ use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
-    let url = spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!("{url}/health_check"))
+        .get(format!("{address}/health_check"))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -15,6 +15,52 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=clever%20jame&email=test_user%40gmail.com";
+
+    let response = client
+        .post(format!("{address}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=clever%20jam", "missing the email."),
+        ("email=test_user%40gmail.com", "missing the name."),
+        ("", "missing the name and email."),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{address}/subscriptions"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {error_message}"
+        );
+    }
 }
 
 fn spawn_app() -> String {
