ff7a7a7cd647dee94978302359fbac170d1f4741
Jaime Liz <lizjaime@gmail.com>
2023-04-19 00:13:29 -0500

    add conffiguration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::net::TcpListener;
-use zero2prod::startup;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    startup::run(address)?.await
+    let config = get_configuration().expect("Failed to read configuration.");
+    let addr = format!("127.0.0.1:{}", config.application_port);
+    let address = TcpListener::bind(addr)?;
+    run(address)?.await
 }
