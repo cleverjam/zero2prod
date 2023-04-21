708624f61faaae08146c9bb1132842a79d29e671
Jaime Liz <lizjaime@gmail.com>
2023-04-21 12:29:21 -0500

    fix: clippy error

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> std::io::Result<()> {
     let addr = format!("127.0.0.1:{}", config.application_port);
     let address = TcpListener::bind(addr)?;
 
-    let db_pool = PgPool::connect(&config.database.connection_string().expose_secret())
+    let db_pool = PgPool::connect(config.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
