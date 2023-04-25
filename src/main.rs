250e7dbe03c12994c76f44b19c67954f2a33a8e1
Jaime Liz <lizjaime@gmail.com>
2023-04-25 18:17:20 -0500

    chore: split config files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 
 use zero2prod::configuration::get_configuration;
@@ -15,11 +16,13 @@ async fn main() -> std::io::Result<()> {
     /* Logging end */
 
     let config = get_configuration().expect("Failed to read configuration.");
-    let addr = format!("127.0.0.1:{}", config.application_port);
-    let address = TcpListener::bind(addr)?;
+    let addr_str = format!("{}:{}", config.application.host, config.application.port);
 
-    let db_pool = PgPool::connect(config.database.connection_string().expose_secret())
-        .await
+    println!("** binding address {}", addr_str);
+    let address = TcpListener::bind(addr_str)?;
+    let db_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(config.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
 
     run(address, db_pool)?.await
