35a5949870ab7cdcee975fd169e6126ea8e51b25
Jaime Liz <lizjaime@gmail.com>
2023-05-06 13:58:08 -0500

    test: add email mock for other /subscribe POST tests

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -6,8 +6,14 @@ use crate::helpers::spawn_app;
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
-
     let body = "name=BoatyMcBoatFace&email=test_user%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
     let response = app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
