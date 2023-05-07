97e83388a409e51ff299652fd65e573f20bc1eb1
Jaime Liz <lizjaime@gmail.com>
2023-05-07 11:45:13 -0500

    test: add application port to TestApp

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -29,6 +29,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 
 pub struct TestApp {
     pub address: String,
+    pub port: u16,
     pub db_pool: PgPool,
     pub email_server: MockServer,
 }
@@ -64,6 +65,7 @@ pub async fn spawn_app() -> TestApp {
         .await
         .expect("Failed to build application");
 
+    let application_port = application.port();
     let address = format!("http://127.0.0.1:{}", application.port());
     let _ = tokio::spawn(application.run_until_stopped());
 
@@ -71,6 +73,7 @@ pub async fn spawn_app() -> TestApp {
         address,
         db_pool: get_connection_pool(&config.database),
         email_server,
+        port: application_port,
     }
 }
 
