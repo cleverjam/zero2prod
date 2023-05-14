5185edcdfe0dd263651235aab78f988beaa6997a
Jaime Liz <lizjaime@gmail.com>
2023-05-13 21:15:01 -0500

    test: subscription confirmation endpoint

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -4,7 +4,7 @@ use wiremock::{Mock, ResponseTemplate};
 use crate::helpers::spawn_app;
 
 #[tokio::test]
-pub async fn invalid_tokens_are_rejected_with_a_400() {
+pub async fn missing_tokens_are_rejected_with_a_400() {
     let app = spawn_app().await;
 
     let response =
@@ -64,4 +64,60 @@ pub async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
     assert_eq!(saved.email, "test_user@gmail.com");
     assert_eq!(saved.name, "BoatyMcBoatFace");
     assert_eq!(saved.status, "confirmed");
+}
+
+#[tokio::test]
+pub async fn unmatched_tokens_are_rejected_with_401() {
+    let app = spawn_app().await;
+    let token = 123;
+    let response = reqwest::get(&format!(
+        "{}/subscriptions/confirm?subscription_token={}",
+        app.address, token
+    ))
+    .await
+    .unwrap();
+
+    assert_eq!(response.status().as_u16(), 401);
+}
+
+#[tokio::test]
+pub async fn confirmation_fails_if_there_is_a_fatal_db_error_retrieving_token()
+{
+    let app = spawn_app().await;
+    let token = 123;
+
+    sqlx::query!(
+        "ALTER TABLE subscription_tokens DROP COLUMN subscription_token",
+    )
+    .execute(&app.db_pool)
+    .await
+    .unwrap();
+
+    let response = reqwest::get(&format!(
+        "{}/subscriptions/confirm?subscription_token={}",
+        app.address, token
+    ))
+    .await
+    .unwrap();
+
+    assert_eq!(response.status().as_u16(), 500);
+}
+#[tokio::test]
+pub async fn confirmation_fails_if_there_is_a_fatal_db_error_upgrading_subscription(
+) {
+    let app = spawn_app().await;
+
+    let body = "name=BoatyMcBoatFace&email=test_user%40gmail.com";
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(email_request);
+
+    sqlx::query!("ALTER TABLE subscriptions DROP COLUMN status",)
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
+
+    assert_eq!(response.status().as_u16(), 500);
 }
